ecd41a9a02794313f4801b63bfde59f622305332
Sam Patel <sampatel379@gmail.com>
2025-05-07 13:59:10 +0100

    Created pseudocode.py and added the TicTacToe class pseudocode

---
+++ pseudocode.py
@@ -0,0 +1,40 @@
+"""
+
+class TicTacToe: 
+    
+    function init()
+        set the game is over to be false
+        create the game board
+        get the user's player choice
+        set the computer's player
+
+
+    function get_user_move()
+        get the user to enter a move
+        while the move is not valid
+            get the user to enter a move
+        return the user move
+    
+        
+    function get_computer_move()
+        generate a random position on the board
+        while the move is not valid
+            generate a random position on the board
+        return the random position
+
+
+    function main_loop()
+        while the game is not over
+            display the board
+            get the user move 
+            add the user move to the board
+            if the user has won
+                display game over message
+                break out of the while loop
+            get the computer's move
+            add the computer's move to the board
+            check if the computer has won
+                end the game
+
+
+"""
