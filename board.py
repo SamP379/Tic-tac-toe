3abb98605e7aa3b978415e45cdfc1751ae5d7ff3
Sam Patel <sampatel379@gmail.com>
2025-05-08 13:12:16 +0100

    Added the check_move() and check_winner() methods to board.py

--- board.py
+++ board.py
@@ -12,17 +12,36 @@ class Board:
                 display_row += "|" + position
             display_row += "|"
             print(display_row)
+        
+    
+    def add_move(self, player : str, position : tuple):
+        row = position[0]
+        column = position[1]
+        self.board[row][column] = player
     
 
-    def check_move(self, move : tuple) -> bool:
-        """Checks if a move is valid. Returns True or False"""
-        row = move[0]
-        column = move[1]
-        try:
-            if (row < 0 or row > 2) or (column < 0 or column > 2):
-                return False
-            elif self.board[row][column] != " ":
-                return False
-            return True
-        except TypeError:
+    def check_move(self, position : tuple) -> bool:
+        row = position[0]
+        column = position[1]
+        if self.board[row][column] != " ":
             return False
+        return True
+    
+
+    def check_winner(self, player : str) -> bool:
+        success_row = player * 3
+        for row in self.board:
+            joined_row = "".join(row)
+            if joined_row == success_row:
+                return True
+        for position in range(len(self.board)):
+            column = ""
+            for row in self.board:
+                column += row[position]
+            if column == success_row:
+                return True
+        primary_diagonal = self.board[0][0] + self.board[1][1] + self.board[2][2]
+        secondary_diagonal = self.board[0][2] + self.board[1][1] + self.board[2][0]
+        if (primary_diagonal == success_row) or (secondary_diagonal == success_row):
+            return True
+        return False
