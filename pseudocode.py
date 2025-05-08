c25b51978e4b2b647c3de9d4867456579fe1ddd5
Sam Patel <sampatel379@gmail.com>
2025-05-08 15:37:24 +0100

    Cleaned up the pseudocode in pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -1,35 +1,60 @@
 """
 
-
 class Board:
 
     function init()
         create the board
     
         
+    function display()
+        clear the screen 
+        display the board
+    
+        
+    function valid_move(position)
+        extract the row from the position
+        extract the column from the position
+        try
+            cast the row to an integer
+            cast the column to an integer
+            if the position is already occupied
+                raise an exception
+        catch any exception
+            return false
+        return true
+
+        
     function add_move(player, position)
         add the player to the board
     
         
-    function check_winner(player)
-    
-        create compare string variable
+    function player_won(player)
+
+        create a string to represent a winning row
 
         for every row in the board
-            join every position in the row into a string
-            if the joined string equals the compare string
+            join every position in the row
+            if the joined row equals the winning row
                 return true    
 
         for every column in the board
-            join every position in the column into a string
-            if the joined string equals the compare string
+            join every position in the column
+            if the joined column equals the winning row
                 return true
         
-        if the primary or seconday diagonal equals the compare string
+        if the primary or seconday diagonal equals the winning row
             return true
         
         return false
+    
         
+    function board_full()
+        for every row in the board
+            for every position in the row
+                if the position equals an empty string
+                    return false
+        return true
+ 
 
 
 
@@ -37,15 +62,14 @@ class Board:
 class TicTacToe: 
     
     function init()
-        set the game state to be true
+        set the game over state to false
         create the game board
         get the user's player choice
         get the computer's player
     
         
     function get_user_player()
-        get the user to enter their player
-        while the user input is not valid
+        while the user player is not valid
             get the user to enter their player
         return the user's player
     
@@ -57,17 +81,31 @@ class TicTacToe:
 
 
     function get_user_move()
-        get the user to enter a move
-        while the move is not valid
+        while the user move is not valid
             get the user to enter a move
-        return the user move
+        return the user's move
     
         
     function get_computer_move()
-        generate a random position on the board
-        while the move is not valid
-            generate a random position on the board
-        return the random position
+        while the computer's move is not valid
+            generate a random move
+        return the computer's move
+    
+    
+    function check_game_over()
+        if the user has won or the computer has won
+            return true
+        elif the board is full
+            return true
+        return false
+    
+        
+    function end_game()
+        if the user won
+            display a you win message
+        else
+            display a you lose message
+        set the game over state to true
 
 
     function main_loop()
@@ -75,14 +113,13 @@ class TicTacToe:
             display the board
             get the user move 
             add the user move to the board
-            if the user has won
-                display a you win message
-                set the game state to false
-            get the computer's move
-            add the computer's move to the board
-            check if the computer has won
-                display a you lose message
-                set the game state to false
+            if the game is over
+                end the game
+            else
+                get the computer move
+                add the computer move to the board
+                if the game is over
+                    end the game
 
-
+                    
 """
