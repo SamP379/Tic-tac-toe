eedcab4493184efe7a2e4ce5a386ea37b43a739e
Sam Patel <sampatel379@gmail.com>
2025-05-09 13:20:53 +0100

    Added docstring comments to the methods in board.py

--- board.py
+++ board.py
@@ -1,12 +1,14 @@
 import os
 
 class Board:
+    """The class to represent the 3x3 grid used in the game Tic Tac Toe"""
 
     def __init__(self):
         self.board = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
     
 
     def display(self):
+        """Displays the the current state of the board"""
         os.system("cls")
         print("")
         for row in self.board:
@@ -19,6 +21,7 @@ class Board:
     
 
     def valid_move(self, position : tuple):
+        """Checks if a move is valid or not, returns True/False"""
         row = position[0]
         column = position[1]
         if (row < 0 or row > 2) or (column < 0 or column > 2):
@@ -29,12 +32,14 @@ class Board:
     
 
     def add_move(self, player : str, position : tuple):
+        """Makes a move for a player, adding their symbol at the given position"""
         row = position[0]
         column = position[1]
         self.board[row][column] = player
     
 
     def player_won(self, player : str) -> bool:
+        """Checks if a given player has won, returns True/False"""
         success_row = player * 3
         for row in self.board:
             joined_row = "".join(row)
@@ -54,6 +59,7 @@ class Board:
     
 
     def board_full(self) -> bool:
+        """Checks if the board is full, returns True/False"""
         for row in self.board:
             for position in row:
                 if position == " ":
