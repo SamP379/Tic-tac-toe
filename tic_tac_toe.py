d6b96c367d71579cf9bf8846a4d58de4d1bce374
Sam Patel <sampatel379@gmail.com>
2025-05-09 10:52:46 +0100

    Added the game_over() method to tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -13,29 +13,25 @@ class TicTacToe:
 
     def get_user_player(self):
         while True:
-            user_player = input("Enter your player (X or O): ").upper()
+            user_player = input("\nEnter your player (X or O): ").upper()
             if user_player in ("X", "O"):
                 break
         return user_player
     
 
     def get_computer_player(self):
-        if self.user_player == "X":
+        if self.user == "X":
             return "O"
         return "X"
     
 
     def get_user_move(self):
         while True:
-            try:
-                row = int(input("Enter row: "))
-                column = int(input("Enter column: "))
-                move = (row, column)
-                if (row < 0 or row > 2) or (column < 0 or column > 2) or (not self.board.check_move(move)):
-                    raise Exception
-                break
-            except Exception:
-                print("Enter a valid move.")
+            row = input("\nEnter row: ")
+            column = input("Enter column: ") 
+            move = (row, column)
+            if self.board.valid_move(move):
+                break 
         return move
     
 
@@ -44,16 +40,24 @@ class TicTacToe:
             row = random.randint(0,2)
             column = random.randint(0,2)
             move = (row,column)
-            if self.board.check_move(move):
+            if self.board.valid_move(move):
                 break
         return move
     
 
-    def end_game(self, user_won : bool):
-        if user_won:
-            print("You win!")
+    def game_over(self):
+        if self.board.player_won(self.user) or self.board.player_won(self.computer):
+            return True
+        elif self.board.board_full():
+            return True
+        return False
+
+
+    def end_game(self):
+        if self.board.player_won(self.user):
+            print("\nYou win!")
         else:
-            print("You lose!")
+            print("\nYou lose!")
         self.game_over = True
     
 
@@ -61,11 +65,11 @@ class TicTacToe:
         while not self.game_over:
             self.board.display()
             user_move = self.get_user_move()
-            self.board.add_move(user_move)
-            if self.board.check_winner(self.user):
-                self.end_game(True)
+            self.board.add_move(self.user, user_move)
+            if self.game_over():
+                self.end_game()
             else:
                 computer_move = self.get_computer_move()
-                self.board.add_move(computer_move)
-                if self.board.check_winner(self.computer):
-                    self.end_game(False)
+                self.board.add_move(self.computer, computer_move)
+                if self.game_over():
+                    self.end_game()
